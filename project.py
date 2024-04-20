16bb41e34643910815a973318d04f40c5c9dffe2
Sam Maschmann <maschmannsam@gmail.com>
2024-04-20 14:01:31 -0500

    no longer reads comments in inputs

--- project.py
+++ project.py
@@ -4,13 +4,21 @@ def satSolve(input):
     return input
 
 if __name__ == "__main__":
-    print("Works")
 
     inFile = open(input("Enter input file: "))
     # When prompted to give a file name in the console, type "./project1-tests/{sat or unsat}/{filename}"
 
     content = inFile.read()
     l = content.split('\n')
+    clist = []
+    
+    for i in l:
+        if(i):
+            if i[0] != 'c':
+                clist.append(i)
+        
+    
+    for i in clist:
+        print(i)
 
-    print(l[1])
     
