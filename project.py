045ec84faccfdd457a8873f4e6e36bcefe96255a
Sam Maschmann <maschmannsam@gmail.com>
2024-04-20 15:55:56 -0500

    added ver basic trivial clause solving Only checks for clauses of length 1

--- project.py
+++ project.py
@@ -1,7 +1,14 @@
 #project.py
 
-def satSolve(numVar, numClause, clauses):
-    M = [1,2,3,-4, -69]
+def satSolve(numVar, numClause, clauses, M):
+    for c in clauses:
+        if (len(c) == 1):
+            if (M[abs(c[0])-1] == 0):
+                M[abs(c[0])-1] = c[0]
+            else:
+                return False, []
+
+
     return True, M
 
 if __name__ == "__main__":
@@ -25,9 +32,26 @@ if __name__ == "__main__":
                 clauses = int(line[3])
 
             elif (i[0] != 'c'):
-                clist.append(i)
-        
-    sat, lits = satSolve(vars, clauses, clist)
+                line = i.split(' ')
+                s = ""
+                for j in line:
+                    if (j != '0'):
+                        s = s + " " + j
+
+                clist.append(s)
+    
+    M = [0]*vars
+
+    cl = []
+    for i in clist:
+        e = i.split(' ')
+        c = []
+        for j in e:
+            if (j != ''):
+                c.append(int(j))
+        cl.append(c)
+
+    sat, lits = satSolve(vars, clauses, cl, M)
     if(sat):
         print("SATISFIABLE")
         s = ""
